ef581a12ea347194e25c4d9d5e9f896d9afdc5ad
Amit kumar yadav <136919680+Amitkumaryadav7714@users.noreply.github.com>
2023-10-05 15:15:01 +0000

    conversion_opratior

--- 01_basic/03_conversion_Oprations.js
+++ 01_basic/03_conversion_Oprations.js
@@ -1,4 +1,49 @@
-let score = 33
-console.log(score)
-console 
-. log  (score)
+let  number = true
+//console.log(number)
+//console.log(typeof number)
+
+let convert = Number(number)
+// console.log( convert)
+
+
+// "33" = 33
+// true = 1, false = 0
+//"232akhfd" = NaN;
+
+//conversion into boolean 
+//let boolean1 = "true"
+//let bool1 = Boolean(boolean1)
+//console.log(boolean1)
+
+// true => true.  type boolean
+// "" => false.
+// "djjd" => true.
+// "1232" => true.
+// "ddf122" => true.
+
+
+//******************************* Opration ****************************************************
+
+let value = 3
+let negValue = -value
+//console.log(negValue)
+
+let a = 4
+let b = 5
+//console.log(a+b)
+
+let str1 = "hello "
+let str2 = "amit"
+let str3 = str1 + str2
+// console.log(str3)           // hello amit
+
+// console.log("1" + 2)        //12
+// console.log(1 + "2")        //12
+// console.log( 1 + 2 + "3")   //33
+// console.log(1 + 2 + "3"+ 1) // 331
+
+//console.log(+true)  //durity conversion
+//console.log(+false)    //durity conversion
+
+
+
