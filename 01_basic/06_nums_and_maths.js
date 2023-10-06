4d27b803d790085c13d4dd8c7744b65574f948f9
Amit kumar yadav <136919680+Amitkumaryadav7714@users.noreply.github.com>
2023-10-06 15:01:29 +0000

    number_maths

---
+++ 01_basic/06_nums_and_maths.js
@@ -0,0 +1,15 @@
+//    node 01_basic/06_nums_and_maths.js
+
+const score = 400
+console.log(score)
+
+const balance = new Number(100)
+console.log(balance);
+
+
+console.log(balance.toString().length)
+   // console.log(balance.)
+   // console.log(balance.)
+    // console.log(balance.)
+   // console.log(balance.)
+
